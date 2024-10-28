1135507674622e381293f02ec980cdd3437c2f3c
Your Name <you@example.com>
2024-10-28 16:09:14 -0300

    em manutenção

--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -1,10 +1,10 @@
 // routes/paymentRoutes.ts
 import express from "express";
-import { createPayment } from "../controllers/paymentController";
+import { createTransparentPayment } from "../controllers/paymentController";
 
 const router = express.Router();
 
 // Rota para criar pagamento
-router.post("/create-payment", createPayment);
+router.post("/create-payment", createTransparentPayment);
 
 export default router;
