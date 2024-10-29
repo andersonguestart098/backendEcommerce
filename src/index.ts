378314a5bb47b7c16a1fefa351024f6b5e2eac60
andersonguestart098 <anderson.guestart98@gmail.com>
2024-10-28 22:25:11 -0300

    formCard

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
     "http://localhost:3001",
-    "https://ecommerce-f06xk1f6x-andersonguestart098s-projects.vercel.app",
+    "https://ecommerce-kwhe21etq-andersonguestart098s-projects.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
