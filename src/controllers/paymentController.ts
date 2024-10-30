247b8f7afb35b3416c87b6abfc9fffd261817053
Your Name <you@example.com>
2024-10-30 15:53:11 -0300

    atualizando

--- src/controllers/paymentController.ts
+++ src/controllers/paymentController.ts
@@ -1,114 +1,202 @@
-import { Request, Response, RequestHandler } from "express";
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { emitOrderStatusUpdate } from "..";
 const mercadopago = require("mercadopago");
 
-mercadopago.configure({
-  access_token: process.env.MERCADO_PAGO_ACCESS_TOKEN || "",
-});
+const prisma = new PrismaClient();
 
-// Define a função como um RequestHandler do Express
-export const createTransparentPayment: RequestHandler = async (
+interface AuthenticatedRequest extends Request {
+  user: {
+    id: string;
+    tipoUsuario: string;
+  };
+}
+
+// Função para listar todos os pedidos (apenas para administradores)
+export const getAllOrders = async (
   req: Request,
   res: Response
-) => {
-  console.log("Iniciando criação de pagamento...");
-  console.log("Dados recebidos no backend:", req.body);
+): Promise<void> => {
+  const authReq = req as AuthenticatedRequest;
+  const userRole = authReq.user.tipoUsuario;
 
-  const {
-    token,
-    transaction_amount,
-    payment_method_id,
-    installments,
-    payer,
-    items,
-    device_id,
-    userId,
-  } = req.body;
-
-  console.log("ID do usuário recebido:", userId);
-  items.forEach((item: any) => {
-    console.log(`Produto: ${item.productId}, Quantidade: ${item.quantity}`);
-  });
-
-  // Log detalhado para verificar os valores dos itens
-  items.forEach((item: any, index: number) => {
-    console.log(
-      `Item ${index} - productId: ${item.productId}, quantity: ${item.quantity}`
-    );
-  });
-
-  // Verificação adicional para garantir que todos os `productId` e `quantity` estão definidos
-  if (!items || !items.every((item: any) => item.productId && item.quantity)) {
-    console.error(
-      "Erro: Um ou mais itens têm `productId` ou `quantity` inválidos."
-    );
-    await res.status(400).json({
-      error: "Cada item deve conter 'productId' e 'quantity' válidos.",
-    });
-    return;
-  }
-
-  const transactionAmount = parseFloat(transaction_amount);
-  if (isNaN(transactionAmount) || transactionAmount <= 0) {
-    await res.status(400).json({
-      error:
-        "O campo 'transaction_amount' é obrigatório e deve ser um número válido.",
+  if (userRole !== "admin") {
+    res.status(403).json({
+      message:
+        "Acesso negado: apenas administradores podem ver todos os pedidos",
     });
     return;
   }
 
   try {
-    // Dados de pagamento para o Mercado Pago
-    const paymentData = {
-      transaction_amount: transactionAmount,
-      token,
-      description: items[0]?.description || "Compra de produtos",
-      installments: Number(installments),
-      payment_method_id,
-      payer: {
-        email: payer.email,
-        first_name: payer.first_name || "",
-        last_name: payer.last_name || "",
-        identification: payer.identification,
+    // Atualiza pedidos onde `shippingCost` é null para 0
+    await prisma.order.updateMany({
+      where: { shippingCost: { equals: null } },
+      data: { shippingCost: 0 },
+    });
+
+    const orders = await prisma.order.findMany({
+      include: {
+        products: {
+          include: {
+            product: true,
+          },
+        },
       },
-      metadata: {
-        device_id: device_id || "default_device_id",
+    });
+
+    res.json(orders);
+  } catch (err) {
+    console.error("Erro ao buscar pedidos:", err);
+    res.status(500).json({ message: "Erro ao buscar pedidos" });
+  }
+};
+
+// Fetch a specific order by ID
+export const getOrderById = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { id } = req.params;
+  const authReq = req as AuthenticatedRequest;
+  const userId = authReq.user.id;
+  const userRole = authReq.user.tipoUsuario;
+
+  try {
+    const order = await prisma.order.findUnique({
+      where: { id },
+      include: {
+        products: {
+          include: {
+            product: true,
+          },
+        },
       },
-      statement_descriptor: "Seu E-commerce",
-      notification_url:
-        "https://ecommerce-fagundes-13c7f6f3f0d3.herokuapp.com/webhooks/mercado-pago/webhook",
-      external_reference: userId,
+    });
+
+    if (!order || (order.userId !== userId && userRole !== "admin")) {
+      res.status(403).json({ message: "Access Denied" });
+      return;
+    }
+
+    // Remove invalid entries
+    const refinedOrder = {
+      ...order,
+      products: order.products.filter(
+        (orderProduct) => orderProduct.product !== null
+      ),
     };
 
-    console.log(
-      "Dados preparados para envio ao Mercado Pago:",
-      JSON.stringify(paymentData, null, 2)
+    res.json(refinedOrder);
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching order" });
+  }
+};
+
+// Função para criar pedido e configurar a preferência de pagamento
+export const createOrder = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const authReq = req as AuthenticatedRequest;
+  const { products, totalPrice, shippingCost = 0 } = req.body; // Define shippingCost como 0 se não fornecido
+  const userId = authReq.user.id;
+
+  try {
+    // Criação do pedido no banco de dados
+    const order = await prisma.order.create({
+      data: {
+        userId,
+        totalPrice,
+        shippingCost,
+        products: {
+          create: products.map((product: any) => ({
+            productId: product.id,
+            quantity: product.quantity,
+          })),
+        },
+      },
+      include: {
+        products: { include: { product: true } },
+      },
+    });
+
+    // Configuração da preferência de pagamento para o Mercado Pago
+    const preference = {
+      items: products.map((product: any) => ({
+        title: product.name,
+        quantity: product.quantity,
+        unit_price: product.price,
+      })),
+      back_urls: {
+        success: "https://seu-front-end.com/order-confirmation",
+        failure: "https://seu-front-end.com/order-failure",
+        pending: "https://seu-front-end.com/order-pending",
+      },
+      auto_return: "approved" as const,
+      statement_descriptor: "Seu E-commerce",
+      external_reference: order.id.toString(),
+    };
+
+    const mercadoPagoResponse = await mercadopago.preferences.create(
+      preference
     );
 
-    const response = await mercadopago.payment.create(paymentData);
-    console.log(
-      "Resposta do Mercado Pago:",
-      JSON.stringify(response.body, null, 2)
-    );
+    res.status(201).json({
+      order,
+      init_point: mercadoPagoResponse.body.init_point,
+    });
+  } catch (err) {
+    console.error("Erro ao criar pedido ou preferência de pagamento:", err);
+    res
+      .status(500)
+      .json({ message: "Erro ao criar pedido ou preferência de pagamento" });
+  }
+};
 
-    if (response.body.status === "approved") {
-      await res.status(200).json({
-        message: "Pagamento aprovado",
-        status: response.body.status,
-        status_detail: response.body.status_detail,
-        id: response.body.id,
-      });
-    } else {
-      await res.status(200).json({
-        message: "Pagamento pendente ou recusado",
-        status: response.body.status,
-        status_detail: response.body.status_detail,
-      });
-    }
-  } catch (error: any) {
-    console.error(
-      "Erro ao criar pagamento:",
-      error.response ? error.response.data : error
-    );
-    await res.status(500).json({ message: "Erro ao criar pagamento", error });
+// Função para atualizar o status do pedido
+export const updateOrderStatus = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { id } = req.params;
+  const { status } = req.body;
+  const authReq = req as AuthenticatedRequest;
+  const userRole = authReq.user.tipoUsuario;
+
+  const validStatuses = [
+    "PENDING",
+    "PAYMENT_APPROVED",
+    "AWAITING_STOCK_CONFIRMATION",
+    "SEPARATED",
+    "DISPATCHED",
+    "DELIVERED",
+    "CANCELED",
+  ];
+
+  if (!validStatuses.includes(status)) {
+    res.status(400).json({ message: "Invalid status" });
+    return;
+  }
+
+  if (userRole !== "admin") {
+    res
+      .status(403)
+      .json({ message: "Access denied: only admins can change order status" });
+    return;
+  }
+
+  try {
+    const order = await prisma.order.update({
+      where: { id },
+      data: { status },
+    });
+
+    emitOrderStatusUpdate(order.id, status, order.userId);
+
+    res.json({ message: "Status updated successfully", order });
+  } catch (err) {
+    console.error("Error updating order status:", err);
+    res.status(500).json({ message: "Error updating order status" });
   }
 };
