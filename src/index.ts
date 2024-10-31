275f6e09835e9ddc26d57bebfa9e391e0daf7787
Your Name <you@example.com>
2024-10-31 13:24:52 -0300

    desabilitando cors

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import { Server as SocketIOServer } from "socket.io";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import { initializeSocket } from "./utils/events"; // Importe a função para inicializar o socket
 import productRoutes from "./routes/productRoutes";
 import bannerRoutes from "./routes/bannerRoutes";
 import userRoutes from "./routes/userRoutes";
@@ -15,48 +14,12 @@ import shippingRoutes from "./routes/shippingRoutes";
 import webhookRoutes from "./routes/webhookRoutes";
 import paymentRoutes from "./routes/paymentRoutes";
 
-const mercadopago = require("mercadopago");
-
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 
-// Configuração do Mercado Pago
-mercadopago.configurations.setAccessToken(
-  process.env.MERCADO_PAGO_ACCESS_TOKEN || ""
-);
-
-// Inicialize o Socket.IO antes de qualquer middleware ou rota
-const io = new SocketIOServer(server, {
-  cors: {
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "https://ecommerce-fagundes-13c7f6f3f0d3.herokuapp.com",
-      "https://ecommerce-qgw68k4ji-andersonguestart098s-projects.vercel.app",
-    ],
-    methods: ["GET", "POST"],
-    credentials: true, // Garantir que cookies e headers de autenticação sejam aceitos
-  },
-  path: "/socket.io",
-});
-
-initializeSocket(io); // Passe o Socket.IO para a função de inicialização
-
-io.on("connection", (socket) => {
-  console.log("User connected");
-
-  socket.on("message", (msg) => {
-    io.emit("message", msg);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-});
-
-// Configurações de CORS robustas
+// Configurações de CORS robustas aplicadas diretamente
 const corsOptions = {
   origin: [
     "http://localhost:3000",
@@ -69,9 +32,7 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions)); // Aplicar CORS antes das rotas
-app.use(express.json());
-
+// Aplica o CORS globalmente antes das rotas
 app.use(cors(corsOptions));
 app.use(express.json());
 
