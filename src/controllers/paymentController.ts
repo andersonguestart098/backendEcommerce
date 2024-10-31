00aad2ad1a6faf11a802bd85a53389ff15fd631c
Your Name <you@example.com>
2024-10-31 17:16:34 -0300

    desabilitando cors

--- src/controllers/paymentController.ts
+++ src/controllers/paymentController.ts
@@ -9,66 +9,38 @@ export const createTransparentPayment = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  console.log("Iniciando criação de pagamento...");
-  console.log("Dados recebidos:", req.body);
+  const { transaction_amount, payment_method_id, payer, description } =
+    req.body;
 
-  const {
+  const paymentData: any = {
     transaction_amount,
     payment_method_id,
-    payer,
-    installments = 1, // Valor padrão de 1 parcela, caso não seja enviado
-    token,
-    items,
-    userId,
-    device_id = "default_device_id",
-  } = req.body;
-
-  if (!transaction_amount || !payment_method_id || !payer || !token) {
-    console.error("Dados obrigatórios ausentes:", {
-      transaction_amount,
-      payment_method_id,
-      payer,
-      token,
-    });
-    res
-      .status(400)
-      .json({ error: "Dados obrigatórios ausentes ou incorretos." });
-    return;
-  }
-
-  const description =
-    items && items.length > 0 ? items[0].description : "Compra de produtos";
-
-  const paymentData = {
-    transaction_amount,
     description,
-    payment_method_id,
-    token,
-    installments,
     payer: {
       email: payer.email,
       first_name: payer.first_name,
-      last_name: payer.last_name || "",
+      last_name: payer.last_name,
       identification: payer.identification,
     },
-    metadata: {
-      device_id: device_id,
-    },
-    statement_descriptor: "Seu E-commerce",
     notification_url:
       "https://ecommerce-fagundes-13c7f6f3f0d3.herokuapp.com/webhooks/mercado-pago/webhook",
-    external_reference: userId,
   };
 
   try {
     const response = await mercadopago.payment.create(paymentData);
-    console.log("Pagamento criado com sucesso:", response.body);
-    res.status(200).json({
-      message: "Pagamento criado",
-      status: response.body.status,
-      status_detail: response.body.status_detail,
-      id: response.body.id,
-    });
+    if (payment_method_id === "pix") {
+      const qrCode =
+        response.body.point_of_interaction.transaction_data.qr_code;
+      const qrCodeUrl =
+        response.body.point_of_interaction.transaction_data.qr_code_url;
+      res.status(200).json({
+        message: "Pix gerado",
+        qr_code: qrCode,
+        qr_code_url: qrCodeUrl,
+      });
+    } else {
+      res.status(200).json({ message: "Pagamento criado", ...response.body });
+    }
   } catch (error: any) {
     console.error("Erro ao criar pagamento:", error.response?.data || error);
     res.status(500).json({
