0555c633aadee899dd24d6f8dc06b59d41f5c398
Your Name <you@example.com>
2024-11-01 11:46:09 -0300

    desabilitando cors

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ const io = new SocketIOServer(server, {
       "http://localhost:3000",
       "http://localhost:3001",
       "https://ecommerce-8tspgehvp-andersonguestart098s-projects.vercel.app",
-      "https://ecommerce-cp4s000f2-andersonguestart098s-projects.vercel.app",
+      "https://ecommerce-mmxqyz0ln-andersonguestart098s-projects.vercel.app",
     ],
     methods: ["GET", "POST"],
     credentials: true,
