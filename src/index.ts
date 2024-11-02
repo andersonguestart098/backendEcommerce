53c7fe92db469ebfdd7946908b17c8f45d260fe7
andersonguestart098 <anderson.guestart98@gmail.com>
2024-11-02 00:41:40 -0300

    atualizando

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ const io = new SocketIOServer(server, {
       "http://localhost:3000",
       "http://localhost:3001",
       "https://ecommerce-8tspgehvp-andersonguestart098s-projects.vercel.app",
-      "https://ecommerce-ir8zmgyoz-andersonguestart098s-projects.vercel.app",
+      "https://ecommerce-9x5e1jp5r-andersonguestart098s-projects.vercel.app",
     ],
     methods: ["GET", "POST"],
     credentials: true,
