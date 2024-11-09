4c9e2758e8a40fdf74dce74419a946d49a8f9c56
andersonguestart098 <anderson.guestart98@gmail.com>
2024-11-08 22:39:35 -0300

    desabilitando cors

--- src/controllers/paymentController.ts
+++ src/controllers/paymentController.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from "express";
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
-
-// Inicializa o Prisma Client
-const prisma = new PrismaClient();
-dotenv.config();
-
 const mercadopago = require("mercadopago");
+
+dotenv.config();
+const prisma = new PrismaClient();
 
 mercadopago.configure({
   access_token: process.env.MERCADO_PAGO_ACCESS_TOKEN || "",
@@ -29,10 +27,10 @@ export const createTransparentPayment = async (
     device_id = "default_device_id",
   } = req.body;
 
-  // Verifique se o transaction_amount é válido
+  // Validação dos dados recebidos
   if (
     !transaction_amount ||
-    transaction_amount <= 0.5 || // Exemplo: mínimo de 0.5 reais para o Mercado Pago
+    transaction_amount <= 0.5 ||
     !payment_method_id ||
     (payment_method_id === "credit_card" && !token) ||
     !userId
@@ -48,19 +46,19 @@ export const createTransparentPayment = async (
       .json({ error: "Dados obrigatórios ausentes ou incorretos." });
     return;
   }
-  
+
   try {
     // Busca o usuário no banco de dados
     const user = await prisma.user.findUnique({
       where: { id: userId },
       include: { address: true },
     });
-    
+
     if (!user) {
       res.status(404).json({ error: "Usuário não encontrado" });
       return;
     }
-    
+
     const payer = {
       email: user.email,
       first_name: user.name.split(" ")[0] || "Nome",
@@ -70,7 +68,7 @@ export const createTransparentPayment = async (
         number: user.cpf || "00000000000",
       },
     };
-    
+
     const description =
       items && items.length > 0 ? items[0].description : "Compra de produtos";
 
@@ -82,7 +80,6 @@ export const createTransparentPayment = async (
         status: "PENDING",
       },
     });
-    
 
     // Dados do pagamento com `order.id` em `external_reference`
     const paymentData: any = {
@@ -95,60 +92,58 @@ export const createTransparentPayment = async (
       },
       statement_descriptor: "Seu E-commerce",
       notification_url:
-        "https://ecommerce-fagundes-13c7f6f3f0d3.herokuapp.com/webhooks/mercado-pago/webhook",
+        "https://seu-ecommerce.com/webhooks/mercado-pago/webhook",
       external_reference: order.id,
     };
-    
+
     if (payment_method_id === "credit_card") {
       paymentData.token = token;
       paymentData.installments = installments;
     }
-    
 
     const response = await mercadopago.payment.create(paymentData);
     console.log("Dados enviados para o Mercado Pago:", paymentData);
 
-
-if (
-  payment_method_id === "bolbradesco" &&
-  response.body.transaction_details?.external_resource_url
-) {
-  res.status(200).json({
-    message: "Pagamento via boleto criado",
-    status: response.body.status,
-    status_detail: response.body.status_detail,
-    id: response.body.id,
-    boleto_url: response.body.transaction_details.external_resource_url,
-  });
-} else if (
-  payment_method_id === "pix" &&
-  response.body?.point_of_interaction?.transaction_data?.qr_code_base64
-) {
-  res.status(200).json({
-    message: "Pagamento via Pix criado",
-    status: response.body.status,
-    status_detail: response.body.status_detail,
-    id: response.body.id,
-    point_of_interaction: {
-      transaction_data: {
-        qr_code_base64:
-          response.body.point_of_interaction.transaction_data.qr_code_base64,
-      },
-    },
-  });
-} else {
-  res.status(200).json({
-    message: "Pagamento criado",
-    status: response.body.status,
-    status_detail: response.body.status_detail,
-    id: response.body.id,
-  });
-}
-} catch (error: any) {
-  console.error("Erro ao criar pagamento:", error.response?.data || error);
-  res.status(500).json({
-    message: "Erro ao criar pagamento",
-    error: error.response?.data,
-  });
-}
+    if (
+      payment_method_id === "bolbradesco" &&
+      response.body.transaction_details?.external_resource_url
+    ) {
+      res.status(200).json({
+        message: "Pagamento via boleto criado",
+        status: response.body.status,
+        status_detail: response.body.status_detail,
+        id: response.body.id,
+        boleto_url: response.body.transaction_details.external_resource_url,
+      });
+    } else if (
+      payment_method_id === "pix" &&
+      response.body?.point_of_interaction?.transaction_data?.qr_code_base64
+    ) {
+      res.status(200).json({
+        message: "Pagamento via Pix criado",
+        status: response.body.status,
+        status_detail: response.body.status_detail,
+        id: response.body.id,
+        point_of_interaction: {
+          transaction_data: {
+            qr_code_base64:
+              response.body.point_of_interaction.transaction_data.qr_code_base64,
+          },
+        },
+      });
+    } else {
+      res.status(200).json({
+        message: "Pagamento criado",
+        status: response.body.status,
+        status_detail: response.body.status_detail,
+        id: response.body.id,
+      });
+    }
+  } catch (error: any) {
+    console.error("Erro ao criar pagamento:", error.response?.data || error);
+    res.status(500).json({
+      message: "Erro ao criar pagamento",
+      error: error.response?.data,
+    });
+  }
 };
